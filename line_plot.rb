77d594f6aa353d6deb916511a409e921db1c069a
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-12-09 14:45:51 +0900

    リファクタリング
      - シングルクォートをダブルクォートに置換．
      - collectをmapに変更．

--- line_plot.rb
+++ line_plot.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/ruby
 
 if ARGV.empty?
-  puts "usage: ./line_plot.rb TITLE X_LABEL Y_LABEL FILE [FILE]..."
+  puts 'usage: ./line_plot.rb TITLE X_LABEL Y_LABEL FILE [FILE]...'
   exit
 end
 
-ENV['BUNDLE_GEMFILE'] = File.expand_path(File.join(File.dirname($0), "Gemfile"))
+ENV['BUNDLE_GEMFILE'] = File.expand_path(File.join(File.dirname($0), 'Gemfile'))
 
 require 'bundler'
 Bundler.require
 
 Gnuplot.open do |gp|
   Gnuplot::Plot.new(gp) do |plot|
-    plot.term "postscript 16 color"
-    plot.output ARGV.first + ".eps"
+    plot.term 'postscript 16 color'
+    plot.output ARGV.first + '.eps'
     plot.title ARGV.shift
     plot.xlabel ARGV.shift
     plot.ylabel ARGV.shift
@@ -23,9 +23,9 @@ Gnuplot.open do |gp|
       File.open(File.expand_path(file)).each_line do |line|
         y << line.to_f
       end
-      x = (0..y.size - 1).collect {|v| v.to_i}
+      x = (0..y.size - 1).map { |v| v.to_i }
       plot.data << Gnuplot::DataSet.new([x, y]) do |ds|
-        ds.with = "lines"
+        ds.with = 'lines'
         ds.linewidth = 4
         ds.title = file
       end
